8f456102c9bbf5ccf0ab8f4e73a71c871eb73b3d
boulouzanacer <boulouza.nacer@gmail.com>
2025-12-10 12:17:20 +0100

    comit 10/12/2025

--- app/src/main/java/com/safesoft/proapp/distribute/gps/LocationSender.java
+++ app/src/main/java/com/safesoft/proapp/distribute/gps/LocationSender.java
@@ -2,7 +2,9 @@ package com.safesoft.proapp.distribute.gps;
 
 import android.content.Context;
 import android.os.StrictMode;
+import android.util.Log;
 
+import com.safesoft.proapp.distribute.databases.DATABASE;
 import com.safesoft.proapp.distribute.eventsClasses.SendLocationEvent;
 import com.safesoft.proapp.distribute.postData.PostData_Bon1;
 import com.safesoft.proapp.distribute.postData.PostData_Bon2;
@@ -13,10 +15,10 @@ import org.json.JSONObject;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationSender {
-
     public static void sendLocation(
             String deviceId,
             double latitude,
@@ -26,11 +28,11 @@ public class LocationSender {
             int nbrBon2,
             int nbrBon1Temp,
             int nbrBon2Temp,
-            List<PostData_Bon1> bon1List,
-            List<PostData_Bon1> bon1TempList,
             Context rContext) {
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+
+        DATABASE controller = new DATABASE(rContext);
 
         try {
             URL url = new URL("https://geo-track.onrender.com/api/bon/sync");
@@ -63,10 +65,134 @@ public class LocationSender {
             json.put("stats", stats);
 
             // BON 1
-            json.put("bon1", BonJsonBuilder.convertBon1List(bon1List, rContext));
+            try {
+                 String querry_bon1 = "SELECT " +
+                        "BON1.RECORDID, " +
+                        "BON1.NUM_BON, " +
+                        "BON1.DATE_BON, " +
+                        "BON1.HEURE, " +
+                        "BON1.DATE_F, " +
+                        "BON1.HEURE_F, " +
+                        "BON1.MODE_RG, " +
+                        "BON1.MODE_TARIF, " +
+
+                        "BON1.NBR_P, " +
+                        "BON1.TOT_QTE, " +
+
+                        "BON1.TOT_HT, " +
+                        "BON1.TOT_TVA, " +
+                        "BON1.TIMBRE, " +
+                        "BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE AS TOT_TTC, " +
+                        "BON1.REMISE, " +
+                        "BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE - BON1.REMISE AS MONTANT_BON, " +
+                        "BON1.MONTANT_ACHAT, " +
+                        "BON1.TOT_HT - BON1.REMISE - BON1.MONTANT_ACHAT AS BENIFICE_BON, " +
+
+                        "BON1.ANCIEN_SOLDE, " +
+                        "BON1.VERSER, " +
+                        "BON1.ANCIEN_SOLDE + (BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE - BON1.REMISE) - BON1.VERSER AS RESTE, " +
+
+                        "BON1.CODE_CLIENT, " +
+                        "Client.CLIENT, " +
+                        "Client.ADRESSE, " +
+                        "Client.WILAYA, " +
+                        "Client.COMMUNE, " +
+                        "Client.TEL, " +
+                        "Client.RC, " +
+                        "Client.IFISCAL, " +
+                        "Client.AI, " +
+                        "Client.NIS, " +
+
+                        "Client.LATITUDE as LATITUDE_CLIENT, " +
+                        "Client.LONGITUDE as LONGITUDE_CLIENT, " +
+
+                        "Client.SOLDE AS SOLDE_CLIENT, " +
+                        "Client.CREDIT_LIMIT, " +
+
+                        "BON1.LATITUDE, " +
+                        "BON1.LONGITUDE, " +
+                        "BON1.LIVRER, " +
+                        "BON1.DATE_LIV, " +
+                        "BON1.IS_IMPORTED, " +
+
+                        "BON1.CODE_DEPOT, " +
+                        "BON1.CODE_VENDEUR, " +
+                        "BON1.EXPORTATION, " +
+                        "BON1.BLOCAGE " +
+                        "FROM BON1 " +
+                        "LEFT JOIN Client ON BON1.CODE_CLIENT = Client.CODE_CLIENT " +
+                        "WHERE IS_EXPORTED = 0 ORDER BY BON1.NUM_BON";
+                ArrayList<PostData_Bon1> bon1List = controller.select_all_bon1_from_database(querry_bon1);
+                json.put("bon1", BonJsonBuilder.convertBon1List(bon1List, rContext));
+            }catch (Exception e){
+                Log.e("LOCATION", "bon 1 : " + e.getMessage());
+            }
+
 
             // BON 1 TEMP
-            json.put("bon1_temp", BonJsonBuilder.convertBon1_Temp_List(bon1TempList, rContext));
+            try {
+                String querry_bon1_temp = "SELECT " +
+                        "BON1_TEMP.RECORDID, " +
+                        "BON1_TEMP.NUM_BON, " +
+                        "BON1_TEMP.DATE_BON, " +
+                        "BON1_TEMP.HEURE, " +
+                        "BON1_TEMP.DATE_F, " +
+                        "BON1_TEMP.HEURE_F, " +
+                        "BON1_TEMP.MODE_RG, " +
+                        "BON1_TEMP.MODE_TARIF, " +
+
+                        "BON1_TEMP.NBR_P, " +
+                        "BON1_TEMP.TOT_QTE, " +
+
+                        "BON1_TEMP.TOT_HT, " +
+                        "BON1_TEMP.TOT_TVA, " +
+                        "BON1_TEMP.TIMBRE, " +
+                        "BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE AS TOT_TTC, " +
+                        "BON1_TEMP.REMISE, " +
+                        "BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE - BON1_TEMP.REMISE AS MONTANT_BON, " +
+                        "BON1_TEMP.MONTANT_ACHAT, " +
+                        "BON1_TEMP.TOT_HT - BON1_TEMP.REMISE - BON1_TEMP.MONTANT_ACHAT AS BENIFICE_BON, " +
+
+                        "BON1_TEMP.ANCIEN_SOLDE, " +
+                        "BON1_TEMP.VERSER, " +
+                        "BON1_TEMP.ANCIEN_SOLDE + (BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE - BON1_TEMP.REMISE) - BON1_TEMP.VERSER AS RESTE, " +
+
+                        "BON1_TEMP.CODE_CLIENT, " +
+                        "CLIENT.CLIENT, " +
+                        "CLIENT.ADRESSE, " +
+                        "CLIENT.WILAYA, " +
+                        "CLIENT.COMMUNE, " +
+                        "CLIENT.TEL, " +
+                        "CLIENT.RC, " +
+                        "CLIENT.IFISCAL, " +
+                        "CLIENT.AI, " +
+                        "CLIENT.NIS, " +
+
+                        "CLIENT.LATITUDE as LATITUDE_CLIENT, " +
+                        "CLIENT.LONGITUDE as LONGITUDE_CLIENT, " +
+
+                        "CLIENT.SOLDE AS SOLDE_CLIENT, " +
+                        "CLIENT.CREDIT_LIMIT, " +
+
+                        "BON1_TEMP.LATITUDE, " +
+                        "BON1_TEMP.LONGITUDE, " +
+                        "BON1_TEMP.LIVRER, " +
+                        "BON1_TEMP.DATE_LIV, " +
+                        "BON1_TEMP.IS_IMPORTED, " +
+
+                        "BON1_TEMP.CODE_DEPOT, " +
+                        "BON1_TEMP.CODE_VENDEUR, " +
+                        "BON1_TEMP.EXPORTATION, " +
+                        "BON1_TEMP.BLOCAGE " +
+                        "FROM BON1_TEMP " +
+                        "LEFT JOIN CLIENT ON BON1_TEMP.CODE_CLIENT = CLIENT.CODE_CLIENT " +
+                        "WHERE IS_EXPORTED = 0 ORDER BY BON1_TEMP.NUM_BON";
+                ArrayList<PostData_Bon1> bon1TempList = controller.select_all_bon1_from_database(querry_bon1_temp);
+                json.put("bon1_temp", BonJsonBuilder.convertBon1_Temp_List(bon1TempList, rContext));
+            }catch (Exception e){
+                Log.e("LOCATION", "bon1_temp error : " + e.getMessage());
+            }
+
 
             // Send
             OutputStream os = conn.getOutputStream();
--- app/src/main/java/com/safesoft/proapp/distribute/gps/services/ServiceSenderLocation.java
+++ app/src/main/java/com/safesoft/proapp/distribute/gps/services/ServiceSenderLocation.java
@@ -1,23 +1,38 @@
 package com.safesoft.proapp.distribute.gps.services;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Handler;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.safesoft.proapp.distribute.R;
+import com.safesoft.proapp.distribute.activities.client.ActivityClientDetail;
 import com.safesoft.proapp.distribute.databases.DATABASE;
 import com.safesoft.proapp.distribute.gps.LocationSender;
 import com.safesoft.proapp.distribute.postData.PostData_Bon1;
@@ -37,121 +52,8 @@ public class ServiceSenderLocation extends Service {
     private String device_name = "";
     private String email = "";
     private String password = "";
-    private DATABASE controller;
-    private String querry_bon1 = "SELECT " +
-            "BON1.RECORDID, " +
-            "BON1.NUM_BON, " +
-            "BON1.DATE_BON, " +
-            "BON1.HEURE, " +
-            "BON1.DATE_F, " +
-            "BON1.HEURE_F, " +
-            "BON1.MODE_RG, " +
-            "BON1.MODE_TARIF, " +
 
-            "BON1.NBR_P, " +
-            "BON1.TOT_QTE, " +
-
-            "BON1.TOT_HT, " +
-            "BON1.TOT_TVA, " +
-            "BON1.TIMBRE, " +
-            "BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE AS TOT_TTC, " +
-            "BON1.REMISE, " +
-            "BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE - BON1.REMISE AS MONTANT_BON, " +
-            "BON1.MONTANT_ACHAT, " +
-            "BON1.TOT_HT - BON1.REMISE - BON1.MONTANT_ACHAT AS BENIFICE_BON, " +
-
-            "BON1.ANCIEN_SOLDE, " +
-            "BON1.VERSER, " +
-            "BON1.ANCIEN_SOLDE + (BON1.TOT_HT + BON1.TOT_TVA + BON1.TIMBRE - BON1.REMISE) - BON1.VERSER AS RESTE, " +
-
-            "BON1.CODE_CLIENT, " +
-            "Client.CLIENT, " +
-            "Client.ADRESSE, " +
-            "Client.WILAYA, " +
-            "Client.COMMUNE, " +
-            "Client.TEL, " +
-            "Client.RC, " +
-            "Client.IFISCAL, " +
-            "Client.AI, " +
-            "Client.NIS, " +
-
-            "Client.LATITUDE as LATITUDE_CLIENT, " +
-            "Client.LONGITUDE as LONGITUDE_CLIENT, " +
-
-            "Client.SOLDE AS SOLDE_CLIENT, " +
-            "Client.CREDIT_LIMIT, " +
-
-            "BON1.LATITUDE, " +
-            "BON1.LONGITUDE, " +
-            "BON1.LIVRER, " +
-            "BON1.DATE_LIV, " +
-            "BON1.IS_IMPORTED, " +
-
-            "BON1.CODE_DEPOT, " +
-            "BON1.CODE_VENDEUR, " +
-            "BON1.EXPORTATION, " +
-            "BON1.BLOCAGE " +
-            "FROM BON1 " +
-            "LEFT JOIN Client ON BON1.CODE_CLIENT = Client.CODE_CLIENT " +
-            "WHERE IS_EXPORTED = 0 ORDER BY BON1.NUM_BON";
-
-    String querry_bon1_temp = "SELECT " +
-            "BON1_TEMP.RECORDID, " +
-            "BON1_TEMP.NUM_BON, " +
-            "BON1_TEMP.DATE_BON, " +
-            "BON1_TEMP.HEURE, " +
-            "BON1_TEMP.DATE_F, " +
-            "BON1_TEMP.HEURE_F, " +
-            "BON1_TEMP.MODE_RG, " +
-            "BON1_TEMP.MODE_TARIF, " +
-
-            "BON1_TEMP.NBR_P, " +
-            "BON1_TEMP.TOT_QTE, " +
-
-            "BON1_TEMP.TOT_HT, " +
-            "BON1_TEMP.TOT_TVA, " +
-            "BON1_TEMP.TIMBRE, " +
-            "BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE AS TOT_TTC, " +
-            "BON1_TEMP.REMISE, " +
-            "BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE - BON1_TEMP.REMISE AS MONTANT_BON, " +
-            "BON1_TEMP.MONTANT_ACHAT, " +
-            "BON1_TEMP.TOT_HT - BON1_TEMP.REMISE - BON1_TEMP.MONTANT_ACHAT AS BENIFICE_BON, " +
-
-            "BON1_TEMP.ANCIEN_SOLDE, " +
-            "BON1_TEMP.VERSER, " +
-            "BON1_TEMP.ANCIEN_SOLDE + (BON1_TEMP.TOT_HT + BON1_TEMP.TOT_TVA + BON1_TEMP.TIMBRE - BON1_TEMP.REMISE) - BON1_TEMP.VERSER AS RESTE, " +
-
-            "BON1_TEMP.CODE_CLIENT, " +
-            "CLIENT.CLIENT, " +
-            "CLIENT.ADRESSE, " +
-            "CLIENT.WILAYA, " +
-            "CLIENT.COMMUNE, " +
-            "CLIENT.TEL, " +
-            "CLIENT.RC, " +
-            "CLIENT.IFISCAL, " +
-            "CLIENT.AI, " +
-            "CLIENT.NIS, " +
-
-            "CLIENT.LATITUDE as LATITUDE_CLIENT, " +
-            "CLIENT.LONGITUDE as LONGITUDE_CLIENT, " +
-
-            "CLIENT.SOLDE AS SOLDE_CLIENT, " +
-            "CLIENT.CREDIT_LIMIT, " +
-
-            "BON1_TEMP.LATITUDE, " +
-            "BON1_TEMP.LONGITUDE, " +
-            "BON1_TEMP.LIVRER, " +
-            "BON1_TEMP.DATE_LIV, " +
-            "BON1_TEMP.IS_IMPORTED, " +
-
-            "BON1_TEMP.CODE_DEPOT, " +
-            "BON1_TEMP.CODE_VENDEUR, " +
-            "BON1_TEMP.EXPORTATION, " +
-            "BON1_TEMP.BLOCAGE " +
-            "FROM BON1_TEMP " +
-            "LEFT JOIN CLIENT ON BON1_TEMP.CODE_CLIENT = CLIENT.CODE_CLIENT " +
-            "WHERE IS_EXPORTED = 0 ORDER BY BON1_TEMP.NUM_BON";
-
+    private Context mContext;
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -176,6 +78,7 @@ public class ServiceSenderLocation extends Service {
         }
 
         startForegroundServiceLogic();
+        mContext = this;
         return START_STICKY;
     }
 
@@ -204,7 +107,8 @@ public class ServiceSenderLocation extends Service {
         locationRunnable = new Runnable() {
             @Override
             public void run() {
-                getLocationAndSend();
+                //getLocationAndSend();
+                getCurrentLocation();
                 handler.postDelayed(this, 1000 * 60); // 15s interval
             }
         };
@@ -219,27 +123,69 @@ public class ServiceSenderLocation extends Service {
     }
 
     private void getLocationAndSend() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
 
         fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
             if (location != null) {
-                Log.e("LOCATION", location.getLatitude() + " / " + location.getLongitude());
-                ArrayList<PostData_Bon1> bon1List = new ArrayList<>();
-                ArrayList<PostData_Bon1> bon1TempList = new ArrayList<>();
-                try {
-                    controller = new DATABASE(this);
-                    bon1List = controller.select_all_bon1_from_database(querry_bon1);
-                    bon1TempList = controller.select_all_bon1_from_database(querry_bon1_temp);
-
-                }catch (Exception e){
-                    Log.e("LOCATION", "bon 1 / bon1_temp error : " + e.getMessage());
-                }
-                LocationSender.sendLocation(deviceId, location.getLatitude(), location.getLongitude(), device_name, 0, 0,0,0, bon1List, bon1TempList, this);
+                Log.e("LOCATION 1", location.getLatitude() + " / " + location.getLongitude());
+                LocationSender.sendLocation(deviceId, location.getLatitude(), location.getLongitude(), device_name, 0, 0,0,0, this);
             }
         });
+    }
+
+
+
+    private void getCurrentLocation() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Vérifie si le GPS est activé
+        if (locationManager == null || !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            AlertDialog dialog = new AlertDialog.Builder(getApplicationContext(),
+                    android.R.style.Theme_Material_Light_Dialog_Alert)
+                    .setTitle("GPS désactivé")
+                    .setMessage("Votre GPS est désactivé. Voulez-vous l’activer ?")
+                    .setPositiveButton("Oui", (d, w) -> {
+                        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        getApplicationContext().startActivity(i);
+                    })
+                    .setNegativeButton("Non", (d, w) -> d.dismiss())
+                    .create();
+
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY); // Needs SYSTEM_ALERT_WINDOW
+            dialog.show();
+            return;
+        }
+
+        // Vérifie la permission
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000).setMaxUpdates(1).setWaitForAccurateLocation(true).build();
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                fusedLocationClient.removeLocationUpdates(this);
+
+                if (locationResult.getLastLocation() == null) {
+                    return;
+                }
+
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    Log.e("LOCATION 2", location.getLatitude() + " / " + location.getLongitude());
+
+                    LocationSender.sendLocation(deviceId, location.getLatitude(), location.getLongitude(), device_name, 0, 0,0,0,  mContext);
+                }
+
+            }
+        }, getMainLooper());
     }
 
     @Override
